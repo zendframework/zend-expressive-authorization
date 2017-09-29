1e5f6cd000b9b1754264db6ff0143974b3a18c47
webimpress <contact@webimpress.com>
2017-09-29 09:04:16 +0100

    Fixed links in PHPDocs to point to proper repository

--- src/Exception/InvalidConfigException.php
+++ src/Exception/InvalidConfigException.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @see       https://github.com/zendframework/zend-expressive-cache for the canonical source repository
+ * @see       https://github.com/zendframework/zend-expressive-authorization for the canonical source repository
  * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-cache/blob/master/LICENSE.md New BSD License
+ * @license   https://github.com/zendframework/zend-expressive-authorization/blob/master/LICENSE.md New BSD License
  */
 
 namespace Zend\Expressive\Authorization\Exception;
