a4e16b2b11d92e10cd75d89f3a1d2e85c2d207c2
webimpress <contact@webimpress.com>
2018-02-27 10:02:39 +0000

    All configuration in ConfigProvider + added tests

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -18,7 +18,46 @@ class ConfigProvider
     {
         return [
             'dependencies'  => $this->getDependencies(),
-            'authorization' => include __DIR__ . '/../config/authorization.php'
+            'authorization' => $this->getAuthorizationConfig(),
+        ];
+    }
+
+    /**
+     * Returns the configuration for the AuthorizationInterface adapter
+     */
+    public function getAuthorizationConfig() : array
+    {
+        return [
+            /**
+             * Example using ZendAcl:
+             *
+             * 'roles' => [
+             *     // insert the role with parent (if any)
+             *     // e.g. 'editor' => ['admin'] (admin is parent of editor)
+             * ],
+             * 'resources' => [
+             *     // an array of resources, as string
+             * ],
+             * 'allow' => [
+             *     // for each role allow some resources
+             *     // e.g. 'admin' => ['admin.pages']
+             * ],
+             * 'deny' => [
+             *     // for each role deny some resources
+             *     // e.g. 'admin' => ['admin.pages']
+             * ]
+             *
+             * Example using ZendRbac:
+             *
+             * 'roles' => [
+             *     // insert the role with parent (if any)
+             *     // e.g. 'editor' => ['admin'] (admin is parent of editor)
+             * ],
+             * 'permissions' => [
+             *     // for each role insert one or more permissions
+             *     // e.g. 'admin' => ['admin.pages']
+             * ]
+             */
         ];
     }
 
@@ -28,6 +67,11 @@ class ConfigProvider
     public function getDependencies() : array
     {
         return [
+            'aliases' => [
+                // Provide an alias for the AuthorizationInterface based on the adapter you are using.
+                // AuthorizationInterface::class => ZendAcl::class,
+                // AuthorizationInterface::class => ZendRbac::class,
+            ],
             'factories'  => [
                 AuthorizationMiddleware::class => AuthorizationMiddlewareFactory::class
             ],
